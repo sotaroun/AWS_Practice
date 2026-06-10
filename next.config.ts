c7ec883c0b440637af0aa058e9fc3ba364ac1e50
sotaroun <acesoichi@yahoo.co.jp>
2026-06-10 13:52:14 +0900

    next.config修正

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+    // 開発環境で許可するオリジンを設定
+  allowedDevOrigins: [
+    '3.113.247.203',  // 現在(6/10)のEC2インスタンスIP
+    'localhost',
+    '127.0.0.1',
+    '0.0.0.0'
+  ]
 };
 
 export default nextConfig;
